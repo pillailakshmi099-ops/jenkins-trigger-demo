db8490f7897c61276d2dddf4b3bff1248e41a583
Lakshmi Pillai <pillailakshmi099@gmail.com>
2026-03-08 19:17:39 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins");
+        System.out.println("Hello from Jenkins for pipeline");
     }
 }
